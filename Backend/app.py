ef129b5b5fff455a77acfc4b56c3f1a951765c46
Oscar <racsodruade0@gmail.com>
2022-01-02 19:25:06 -0600

    Implementación de carga y almacenamiento en Backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,29 +1,83 @@
+import json
+from os import replace
 from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
-
-from math import pow
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origin": "*"}})#habilita el acceso a la api desde rutas externas
 
 biblioteca = []
+listas = []
+
+artistas=[]
+albums = []
+canciones = []
 
 @app.route('/carga', methods=['POST'])
-def add_usuario():  
-    new_song = {
-        "artista": request.json['username'],
-        "vecesReproducida": request.json['vecesReproducida'],
-        "album": request.json['album'],
-        "imagen": request.json['imagen'],
-        "url": request.json['url'],
-        "nombre": request.json['nombre']
-    }
-    biblioteca.append(new_song)            
-    return jsonify({'ok':True, "Usuario": "Usuario agreado satisfactoriamente"})
+def add_usuario():   
+    a = request.json    
+    for arts1 in a :                
+        art1 = arts1["artista"]                       
+        artista = {            
+            "artista":art1,
+            } 
+        if artistas == []:       
+            artistas.append(artista)
+        else:
+            b = len(artistas) - 1
+            if art1 not in artistas[b]['artista']:
+                artistas.append(artista)
+    
+    for arts2 in a :                
+        art2 = arts2["artista"]
+        alb2 = arts2["album"]
+        
+        
+        album = {
+            "album":alb2,      
+            "artista":art2,  
+        }
+
+        if albums == []:
+            albums.append(album)
+        else:
+            c = len(albums) - 1
+            if alb2 not in albums[c]['album']:
+                albums.append(album)
+                
+            
+    for arts3 in a :                
+        art3 = arts3["artista"]
+        alb3 = arts3["album"]
+        rep3 = arts3["vecesReproducida"]
+        img3 = arts3["imagen"]
+        rut3 = arts3["ruta"]
+        nom3 = arts3["nombre"] 
+        
+        im = img3.replace('\\','//')
+        ru = rut3.replace('\\','//')
+        cancion = {            
+            "artista":art3,
+            "album":alb3,  
+            "vecesReproducida":rep3,      
+            "imagen":im,
+            "nombre":nom3,
+            "ruta":ru,     
+        }        
+        if canciones == []:
+            canciones.append(cancion)
+        else:
+            d = len(canciones) - 1
+            if nom3 not in canciones[d]['nombre']:
+                canciones.append(cancion)       
+
+
+    print(canciones)
+    return jsonify({"Biblioteca": "Lista agreada satisfactoriamente"})
 
 @app.route('/mostrar')
 def listado():    
-    return jsonify(biblioteca)
+    return jsonify(canciones)
 
 @app.route('/csv', methods=['GET'])
 def get_datos():
--- Frontend/IPCMusic/views.py
+++ Frontend/IPCMusic/views.py
@@ -3,37 +3,25 @@ from django.shortcuts import render, redirect
 import requests
 from django.views.decorators.csrf import csrf_protect
 from IPCMusic.forms import *
-# Create your views here.
+import csv
+import io
+import xml.etree.ElementTree as ET
 
-from django.http import HttpResponse
-from django.template.loader import get_template
-
+global csv_list
+csv_list = []
 endpoint = 'http://localhost:4000{}'
 @csrf_protect
 
 def inicio(request):
-    plantilla_inicio = get_template('inicio.html')
-    rend_plantilla = plantilla_inicio.render()
-
-    return HttpResponse(rend_plantilla)
-
-"""def carga_csv(request):
     if request.method == 'GET':
-        url = endpoint.format('/csv')  # http://localhost:4000/datos
+        url = endpoint.format('/mostrar')  # http://localhost:4000/datos
         data = requests.get(url)  # consulta a la API
         context = {
-            'data': data.text,
+            'data': data.json,
         }
-        return render(request, 'csv.html', context)
-    elif request.method == 'POST':
-        docs = request.FILES['document']
-        data = docs.read()
-        url = endpoint.format('/csv')
-        requests.post(url, data)
-        return redirect('CSV')"""
-import csv
-import io
-import xml.etree.ElementTree as ET
+        return render(request, 'inicio.html', context)    
+
+root = "Output.xml"
 def carga_csv(request):
     if request.method == 'GET':
         return render(request, 'csv.html')
@@ -45,9 +33,7 @@ def carga_csv(request):
              return render(request, 'csv.html',{'text_error':'Seleccione un documento valido'})              
        
         data = docs.read().decode('utf-8')
-        io_string = io.StringIO(data)
-        global csv_list
-        csv_list = []
+        io_string = io.StringIO(data)        
         val=""
         error_csv = True
         
@@ -74,8 +60,7 @@ def carga_csv(request):
                             imagen.text = line['imagen']
                             ruta = ET.SubElement(cancion, "ruta")
                             ruta.text = line['ruta']
-                            error_csv = False
-                            
+                            error_csv = False                            
                             
                         else:
                             val = ertxt+str(cont)+" nombre: "+line['nombre']+" se requiere un texto."
@@ -92,39 +77,33 @@ def carga_csv(request):
             else:
                 val = ertxt+str(cont)+" artista: "+line['artista']+" se requiere texto"
                 error_csv = True
-                csv_list.append(val)
-        root = "Output.xml"
+                csv_list.append(val)        
         tree = ET.ElementTree(usrconfig)
-        tree.write(root, encoding='utf-8', xml_declaration=True)
-        
+        tree.write(root, encoding='utf-8', xml_declaration=True)        
         contenido = open(root).read()
         biblioteca = ET.fromstring(contenido)
         for biblio in biblioteca.iter("ListasReproduccion"):
             for can in biblio.iter("cancion"):
-                nombre = can.attrib['nombre']
-                album = ""
-                artista = ""
-                veces_rep = ""
-                imagen = ""
-                ruta = ""
-                for ar in can.iter("artista"):
-                    artista += ar.text  
-                for al in can.iter("album"):
-                    album += al.text
-                for vr in can.iter("vecesReproducida"):
-                    veces_rep += vr.text       
-                for im in can.iter("imagen"):
-                    imagen += im.text 
-                for ru in can.iter("ruta"):
-                    ruta += ru.text   
-                agregar_cancion = ({"artista":artista,"vecesReproducida":veces_rep,"album":album,"imagen":imagen,"ruta":ruta,"nombre":nombre})
-                csv_list.append(agregar_cancion)    
-        context = {
-            'data': csv_list,
-            'text_error':val,
-            'error':error_csv,
-        }
-                
+                    nombre = can.attrib['nombre']
+                    album = ""
+                    artista = ""
+                    veces_rep = ""
+                    imagen = ""
+                    ruta = ""
+                    for ar in can.iter("artista"):
+                        artista += ar.text  
+                    for al in can.iter("album"):
+                        album += al.text
+                    for vr in can.iter("vecesReproducida"):
+                        veces_rep += vr.text       
+                    for im in can.iter("imagen"):
+                        imagen += im.text 
+                    for ru in can.iter("ruta"):
+                        ruta += ru.text   
+                    agregar_cancion = ({"artista":artista,"vecesReproducida":veces_rep,"album":album,"imagen":imagen,"ruta":ruta,"nombre":nombre})
+                    csv_list.append(agregar_cancion) 
+        context = {'text_error':val,
+            'error':error_csv}                
         if error_csv is True:
             error_csv = False  
             return render(request, 'csv.html', context)  
@@ -138,95 +117,46 @@ def carga_csv(request):
 #--------------------------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------
 
-
-def enviar_json(request):
-    context = {
-            'data': csv_list,
-        }
-    return render(request, 'csv_xml.html', context)
-
-
 def carga_xml(request):
     if request.method == 'GET':
-        url = endpoint.format('/xml')  # http://localhost:4000/datos
-        data = requests.get(url)  # consulta a la API
-        context = {
-            'data': data.text,
-        }
-        return render(request, 'xml.html', context)
+        return render(request, 'xml.html')
     elif request.method == 'POST':
-        docs = request.FILES['document']
-        data = docs.read()
-        url = endpoint.format('/xml')
-        requests.post(url, data)
-        return redirect('XML')
+        ertxt = "Error en la linea: "
+        try:
+            docs = request.FILES['document']
+        except:
+            return render(request, 'xml.html',{'text_error':'Seleccione un documento valido'})
+        data = docs.read().decode('utf-8')
+        biblioteca = ET.fromstring(data)
+        for biblio in biblioteca.iter("ListasReproduccion"):
+            for can in biblio.iter("cancion"):
+                    nombre = can.attrib['nombre']
+                    album = ""
+                    artista = ""
+                    veces_rep = ""
+                    imagen = ""
+                    ruta = ""
+                    for ar in can.iter("artista"):
+                        artista += ar.text  
+                    for al in can.iter("album"):
+                        album += str(al.text)
+                    for vr in can.iter("vecesReproducida"):
+                        veces_rep += vr.text       
+                    for im in can.iter("imagen"):
+                        imagen += im.text 
+                    for ru in can.iter("ruta"):
+                        ruta += ru.text   
+                    agregar_cancion = ({"artista":artista,"vecesReproducida":veces_rep,"album":album,"imagen":imagen,"ruta":ruta,"nombre":nombre})
+                    csv_list.append(agregar_cancion) 
+    return redirect('CSV_XML')
 
-
-"""def carga_xmlcopleto(ruta):   
-    contenido = open(ruta).read()
-    biblioteca = ET.fromstring(contenido)
-    for biblio in biblioteca.iter("biblioteca"):
-        for can in biblio.iter("cancion"):
-            nombre = can.attrib['nombre']
-            album = ""
-            artista = ""
-            imagen = ""
-            ruta = ""
-            for ar in can.iter("artista"):
-                artista += ar.text  
-            for al in can.iter("album"):
-                album += al.text  
-            for im in can.iter("imagen"):
-                imagen += im.text 
-            for ru in can.iter("ruta"):
-                ruta += ru.text   
-            Listar.agregarCancion(artista,album,imagen,ruta,nombre)"""
- 
-
-def carga_masiva():
-    print("Hola carga Masiva")
-
-
-
-
-# Create your views here.
-
-
-
-
-
-def reports(request):
+def enviar_json(request):    
     if request.method == 'GET':
-        date = request.GET.get('date', None)
-        code = request.GET.get('code', None)
+        context = {'data': csv_list}
+        return render(request, 'csv_xml.html', context)
+    elif request.method == 'POST':        
+        url = endpoint.format('/carga')
+        requests.post(url,json=csv_list) 
+        csv_list.clear()      
+        return redirect("Inicio")
 
-        context = {
-            'date': None,
-            'code': None,
-        }
-        if date is not None:
-            context['date'] = date
-
-        if code is not None:
-            context['code'] = code
-        return render(request, 'reports.html', context)
-
-
-def calc(request):
-    if request.method == 'GET':
-        num_1 = request.GET.get('num_1', 0)
-        num_2 = request.GET.get('num_2', 0)
-
-        url = endpoint.format('/potencia')
-
-        potencia = requests.get(url, {
-            'num_1': num_1,
-            'num_2': num_2,
-        })
-
-        context = {
-            'potencia': potencia.text,
-        }
-
-        return render(request, 'calc.html', context)
-    
